b77fa708c49a658969bce62ae544fff4f62e018c
Judith Weintraub <93940153+judy318@users.noreply.github.com>
2022-01-13 01:20:11 +0000

    part1

--- part1.py
+++ part1.py
@@ -16,12 +16,19 @@ The smallest number is 2
 '''
 
 number = int(input("Enter a number: "))
-
-smallest = number
-
-number = int(input("Enter another number: "))
-
-if number < smallest:
-  smallest = number
-  
-print("The smallest number is ", smallest)
+number1 = int(input("Enter another number: "))
+number2 = int(input("Enter another number: "))
+if number < number1 and number < number2:
+  print("The smallest number is ", number)
+elif number1 < number2 and number1 < number:
+  print("The smallest number is ", number1)
+elif number2 < number1 and number2 < number:
+  print("The smallest number is ", number1)
+elif number1 == number and number < number2:
+  print("The smallest number is ", number1)
+elif number1 == number2 and number1 < number:
+  print("The smallest number is ", number1)
+elif number2 == number and number2 < number:
+  print("The smallest number is ", number1)
+elif number1 == number and number1 == number2:
+  print("The smallest number is ", number1)
